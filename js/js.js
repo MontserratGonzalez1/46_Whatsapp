c116975ea27e8922cb3e0b982f56d8cc4206af5b
MontserratGonzalez1 <monkygs@gmail.com>
2016-11-25 16:59:33 -0600

    usando materialize para hacer Whatsapp

---
+++ js/js.js
@@ -0,0 +1,8 @@
+$('.button-collapse').sideNav({
+      menuWidth: 0, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+
